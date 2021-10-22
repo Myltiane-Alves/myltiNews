5915fb0f148ec3afdab1702d708a0b426d0bea08
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-22 10:59:06 -0300

    Salvando usuários no banco de dados

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { query as q } from "faunadb";
 
 import  NextAuth from "next-auth";
+
 import Providers from "next-auth/providers";
+import { fauna } from "../../../services/fauna";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default NextAuth({
@@ -13,8 +16,21 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
 
-  database: process.env.DATABASE_URL,
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data: { email }}
+        )
+      )
+
+      return true;
+    },
+  }
+
 
 })  
 
