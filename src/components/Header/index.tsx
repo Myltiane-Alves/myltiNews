f0b011213f5df3b7ae6fc06f80457bc2442f5261
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-28 16:09:26 -0300

    Adicionando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,23 @@
-/* eslint-disable @next/next/no-img-element */
-import { SignInButton } from '../SignInButton';
+import { SignInButton } from '../SignInButton'
+import { ActiveLink }  from '../ActiveLink';
 
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
 export function Header() {
-    return(
-        <header className={styles.headerContainer}>
-          <div className={styles.headerContent}>
-            <img src="/images/logo.svg" alt="ig.news" />
-            <nav>
-                <a className={styles.active}>Home</a>    
-                <a>Posts</a>    
-            </nav>  
-
-            <SignInButton />
-          </div>  
-        </header>
-    );
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <ActiveLink  activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink  prefetch activeClassName={styles.active} href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  )
 }
